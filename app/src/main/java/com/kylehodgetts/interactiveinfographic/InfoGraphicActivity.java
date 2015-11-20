e2a75746f95d081d174ed2f71bd19e9db81ac373
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-11-20 21:12:26 +0000

    Initial Commit

---
+++ app/src/main/java/com/kylehodgetts/interactiveinfographic/InfoGraphicActivity.java
@@ -0,0 +1,13 @@
+package com.kylehodgetts.interactiveinfographic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoGraphicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infographic);
+    }
+}
