96b26bc6012094c4a62d02b6358baba3c32b1207
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-12-07 12:44:20 +0000

    removed log import

--- app/src/main/java/com/kylehodgetts/interactiveinfographic/view/ComboChartFragment.java
+++ app/src/main/java/com/kylehodgetts/interactiveinfographic/view/ComboChartFragment.java
@@ -3,7 +3,6 @@ package com.kylehodgetts.interactiveinfographic.view;
 import android.app.Fragment;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
