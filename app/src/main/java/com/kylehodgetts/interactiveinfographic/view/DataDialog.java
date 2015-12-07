fd7436d66941d6c01134f6cc3788b71c33761a91
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-12-07 14:00:25 +0000

    Added icons

--- app/src/main/java/com/kylehodgetts/interactiveinfographic/view/DataDialog.java
+++ app/src/main/java/com/kylehodgetts/interactiveinfographic/view/DataDialog.java
@@ -2,7 +2,6 @@ package com.kylehodgetts.interactiveinfographic.view;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
